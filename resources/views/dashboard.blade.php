baa4f8e474f262f591d16537cc9627f4f97d6a17
Alvin Merto <ajbmerto@gmail.com>
2024-11-27 17:57:45 +0800

    new update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,65 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="m-grid__item m-grid__item--fluid m-wrapper">
+					<!-- BEGIN: Subheader -->
+					<div class="m-subheader ">
+						<div class="d-flex align-items-center">
+							<div class="mr-auto">
+								<h3 class="m-subheader__title ">
+									Dashboard
+								</h3>
+							</div>
+							<div>
+								<span class="m-subheader__daterange" id="m_dashboard_daterangepicker">
+									<span class="m-subheader__daterange-label">
+										<span class="m-subheader__daterange-title"></span>
+										<span class="m-subheader__daterange-date m--font-brand"></span>
+									</span>
+									<a href="#" class="btn btn-sm btn-brand m-btn m-btn--icon m-btn--icon-only m-btn--custom m-btn--pill">
+										<i class="la la-angle-down"></i>
+									</a>
+								</span>
+							</div>
+						</div>
+					</div>
+					<!-- END: Subheader -->
+					<div class="m-content">
+						<!--Begin::Main Portlet-->
+						<div class="row">
+							<div class="col-xl-4">
+								<!--begin:: Widgets/Quick Stats-->
+								<div class="row m-row--full-height">
+									<div class="col-sm-4 col-md-4 col-lg-4">
+										<div class="m-portlet m-portlet--half-height m-portlet--border-bottom-brand ">
+											<div class="m-portlet__body">
+												<div class="m-widget26">
+													<div class="m-widget26__number">
+														570
+														<small>
+															All Sales
+														</small>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+                                    <div class="col-sm-4 col-md-4 col-lg-4">
+										<div class="m-portlet m-portlet--half-height m-portlet--border-bottom-brand ">
+											<div class="m-portlet__body">
+												<div class="m-widget26">
+													<div class="m-widget26__number">
+														570
+														<small>
+															All Sales
+														</small>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+                            	</div>
+								<!--end:: Widgets/Quick Stats-->
+							</div>
+                        </div>
+                    </div>
+							
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,26 +11,62 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!--begin::Web font -->
+		<script src="https://cdn.bootcss.com/webfont/1.6.16/webfontloader.js"></script>
+		<script>
+            WebFont.load({
+                google: {"families":["Montserrat:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+                active: function() {
+                    sessionStorage.fonts = true;
+                }
+            });
+		</script>
+		<!--end::Web font -->
+        <!--begin::Base Styles -->  
+        <!--begin::Page Vendors -->
+		<link href="{{asset('vendors')}}/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Page Vendors -->
+		<link href="{{asset('vendors')}}/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="{{asset('demo')}}/demo3/base/style.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Base Styles -->
+		<link rel="shortcut icon" href="{{asset('demo')}}/demo3/media/img/logo/favicon.ico" />
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
+        <!-- <div class="min-h-screen bg-gray-100"> -->
+        <div class="m-grid m-grid--hor m-grid--root m-page">
+           
             <!-- Page Heading -->
-            @isset($header)
+            <!-- @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endisset -->
 
             <!-- Page Content -->
+            
             <main>
-                {{ $slot }}
+                <div class="m-grid m-grid--hor m-grid--root m-page">
+                    @include('layouts.navigation')
+                    <div class="m-grid__item m-grid__item--fluid m-wrapper">
+                        {{ $slot }}
+                    </div>
+                </div>
             </main>
+
         </div>
+
+        <script src="{{asset('vendors')}}/base/vendors.bundle.js" type="text/javascript"></script>
+		<script src="{{asset('demo')}}/demo3/base/scripts.bundle.js" type="text/javascript"></script>
+		<!--end::Base Scripts -->   
+        <!--begin::Page Vendors -->
+		<script src="{{asset('vendors')}}/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+		<!--end::Page Vendors -->  
+        <!--begin::Page Snippets -->
+		<script src="{{asset('app')}}/js/dashboard.js" type="text/javascript"></script>
     </body>
 </html>
